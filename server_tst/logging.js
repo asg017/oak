4063569fcbd34d097cdcdbbb8c2cb7c7dfa46ab6
Alex Garcia <alexsebastian.garcia@gmail.com>
2019-04-21 06:29:44 +0000

    added logging

---
+++ server_tst/logging.js
@@ -0,0 +1,22 @@
+const winston = require("winston");
+const { timeFormat } = require("d3-time-format");
+
+const logDate = timeFormat("%Y-%m-%d %H-%M-%S.%L");
+const createLogger = config => {
+  const { label = "UNNAMED" } = config;
+  return winston.createLogger({
+    level: "info",
+    format: winston.format.combine(
+      winston.format.label({ label }),
+      winston.format.prettyPrint(),
+      winston.format.colorize(),
+      winston.format.timestamp(),
+      winston.format.printf(
+        ({ level, message, label, timestamp }) =>
+          `[${logDate(new Date(timestamp))}] [${label}] ${level} : ${message}`
+      )
+    ),
+    transports: [new winston.transports.Console()]
+  });
+};
+module.exports = { createLogger };
--- server_tst/oak.js
+++ server_tst/oak.js
@@ -3,6 +3,10 @@ const { Runtime } = require("@observablehq/runtime");
 const { Library } = require("@observablehq/stdlib");
 const { spawn } = require("child_process");
 const { OakInspector } = require("./oak-inspector.js");
+const { createLogger } = require("./logging.js");
+
+const socketLogger = createLogger({ label: "Socket" });
+const oakLogger = createLogger({ label: "Oak" });
 
 const Generators = new Library().Generators;
 const server = require("http").createServer();
@@ -10,15 +14,15 @@ const io = require("socket.io")(server);
 const port = 3000;
 
 io.on("connection", socket => {
-  console.log(`connection made ${socket.id}`);
+  socketLogger.info(`connection made ${socket.id}`);
   socket.emit("oakfile", Oak);
   socket.on("disconnect", socket => {
-    console.log("disconnected!");
+    socketLogger.info(`${socket.id} disconnected!`);
   });
 });
 
 server.listen(port, () =>
-  console.log(`socket.io update server listening on ${port}!`)
+  socketLogger.info(`socket.io update server listening on ${port}!`)
 );
 
 const loadOakfile = (config = {}) => {
@@ -58,7 +62,7 @@ let Oak;
 loadOakfile().then(oak => {
   Object.keys(oak.variables).map(key => {
     const variable = oak.variables[key];
-    console.log(variable);
+    oakLogger.info(variable);
     const { recipe, filename, deps } = variable;
     m.variable(new OakInspector(io, key)).define(
       key,
@@ -66,21 +70,23 @@ loadOakfile().then(oak => {
       function() {
         const invalidation = arguments[arguments.length - 1];
         let process;
+        let processLogger = oakLogger.child({ label: `process-${key}` });
         return Generators.observe(change => {
           const watch = (curr, prev) => {
-            console.log(`${key} file update???`);
+            oakLogger.info(`${key} file update???`);
             change(key);
           };
           fs.watchFile(filename, { persistent: false, interval: 500 }, watch);
 
           invalidation.then(() => {
-            console.log(`${key} invalidated`);
+            oakLogger.info(`${key} invalidated`);
             fs.unwatchFile(filename, watch);
             if (process) process.kill();
           });
           process = spawn(recipe, { shell: true });
 
           process.stdout.on("data", chunk => {
+            processLogger.info(chunk);
             io.emit("chunk", { key, chunk });
           });
 
