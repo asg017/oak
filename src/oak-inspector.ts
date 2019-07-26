4573212a340f7bfa79c3ce5616bf632388652060
Alex Garcia <alexsebastian.garcia@gmail.com>
2019-07-26 07:49:58 +0000

    working static?

--- src/oak-inspector.ts
+++ src/oak-inspector.ts
@@ -7,21 +7,21 @@ export class OakInspector {
   constructor(socket, initialName) {
     this.socket = socket;
     this.initialName = initialName;
-    logger.debug(`CustomInspector constructor called`);
+    // logger.debug(`CustomInspector constructor called`);
   }
 
   pending() {
-    logger.debug(`${this.initialName} pending called`);
+    // logger.debug(`${this.initialName} pending called`);
     if (this.socket)
       this.socket.emit("pending", { initialName: this.initialName });
   }
 
-  fulfilled(value:any, name:string) {
-    logger.debug(`fulfilled called, ${value}|${name}`);
+  fulfilled(value: any, name: string) {
+    logger.debug(`fulfilled called, |${name}`);
     if (this.socket) this.socket.emit("fulfilled", { value, name });
   }
 
-  rejected(error:any, name:string) {
+  rejected(error: any, name: string) {
     logger.error(`rejected called, ${error}|${name}`);
     if (this.socket) this.socket.emit("rejected", { error, name });
   }
--- src/oak-static.ts
+++ src/oak-static.ts
@@ -5,34 +5,114 @@ import { createLogger } from "./logging";
 import { getStat, loadOakfile } from "./utils";
 import { EventEmitter } from "events";
 
-import { OakType } from "./types";
+import { OakVariableType, OakType } from "./types";
 
+type StatType = {
+  mtime: number;
+};
+
+type OakVariableWithStat = {
+  oakVariable: OakVariableType;
+  stat: StatType;
+};
 const oakLogger = createLogger({ label: "Oak" });
+
+const runRecipe = (recipe: string) => {
+  const e = new EventEmitter();
+  const process = spawn(recipe, { shell: true });
+  console.log(`running recipe ${recipe}`);
+  process.stdout.on("data", chunk => {
+    e.emit("stdout", chunk);
+  });
+  process.stderr.on("data", chunk => {
+    e.emit("stderr", chunk);
+  });
+
+  process.on("close", async code => {
+    e.emit("close", code);
+  });
+  process.on("error", () => {
+    e.emit("error");
+  });
+  return e;
+};
+
+function performRecipe(
+  oakVariable: OakVariableType
+): Promise<OakVariableWithStat> {
+  return new Promise((res, rej) => {
+    runRecipe(oakVariable.recipe)
+      .on("stdout", chunk => {})
+      .on("stderr", chunk => {})
+      .on("close", async code => {
+        console.info(`Process closing with code ${code}`);
+        const stat = await getStat(oakVariable.filename).catch(e => {
+          oakLogger.error(
+            `Error reading stat for ${oakVariable.filename} (2nd attempt): ${e}`
+          );
+          throw e;
+          return null;
+        });
+        res({ oakVariable, stat });
+      })
+      .on("error", () => {
+        process = null;
+        console.error(`Process errored`);
+        rej("error");
+      });
+  });
+}
+
+function createVariableDefinition(
+  oakVariable: OakVariableType,
+  key: string
+): (...variableDependenciesX: OakVariableWithStat[]) => Promise<object> {
+  return async function(...variableDependencies: OakVariableWithStat[]) {
+    let process;
+    let processLogger = oakLogger.child({ label: `process-${key}` });
+
+    let stat = await getStat(oakVariable.filename).catch(e => {
+      // TODO check error code, only "file not exists" error
+      oakLogger.error(
+        `Error reading stat for ${oakVariable.filename} (intial): ${e}`
+      );
+      return null;
+    });
+    // ideally, this would only happen at most once, because the target file doesnt
+    // exist yet. after running the recipe, the target should exist
+    if (stat === null) {
+      oakLogger.info(
+        `[${key}] running recipe - bc inital stat not available - "${
+          oakVariable.recipe
+        }"`
+      );
+      return performRecipe(oakVariable);
+    }
+
+    const updatedDeps = variableDependencies.filter(
+      dep => dep.stat.mtime > stat.mtime
+    );
+    if (updatedDeps.length > 0) {
+      oakLogger.debug(
+        `${key} is out of date because ${
+          updatedDeps.length
+        } dependenices (${updatedDeps
+          .map(dep => dep.oakVariable.filename)
+          .join(",")}) have updated. Calling recipe '${oakVariable.recipe}'`
+      );
+      return performRecipe(oakVariable);
+    }
+    return new Promise(function(res, rej) {
+      res({ oakVariable, stat });
+    });
+  };
+}
 
 export async function oak_static(argv) {
   const runtime = new Runtime();
   const inspector = new OakInspector(null, "default");
   const m = runtime.module();
 
-  const runRecipe = (recipe: string) => {
-    const e = new EventEmitter();
-    const process = spawn(recipe, { shell: true });
-    console.log(`running recipe ${recipe}`);
-    process.stdout.on("data", chunk => {
-      e.emit("stdout", chunk);
-    });
-    process.stderr.on("data", chunk => {
-      e.emit("stderr", chunk);
-    });
-
-    process.on("close", async code => {
-      e.emit("close", code);
-    });
-    process.on("error", () => {
-      e.emit("error");
-    });
-    return e;
-  };
   let Oak: OakType | null;
 
   console.log("TODO in oak_static, argv:");
@@ -42,92 +122,11 @@ export async function oak_static(argv) {
   Oak = oak;
   Object.keys(oak.variables).map(key => {
     const variable = oak.variables[key];
-    const { recipe, filename, deps } = variable;
-    m.variable(new OakInspector(null, key)).define(key, deps, async function(
-      variableDependencies
-    ) {
-      let process;
-      let processLogger = oakLogger.child({ label: `process-${key}` });
-
-      let stat = await getStat(variable.filename).catch(e => {
-        // TODO check error code, only "file not exists" error
-        oakLogger.error(
-          `Error reading stat for ${variable.filename} (intial): ${e}`
-        );
-        return null;
-      });
-
-      // ideally, this would only happen at most once, because the target file doesnt
-      // exist yet. after running the recipe, the target should exist
-      if (stat === null) {
-        oakLogger.info(
-          `[${key}] running recipe - bc inital stat not available - "${recipe}"`
-        );
-        return new Promise((res, rej) => {
-          process = runRecipe(recipe)
-            .on("stdout", chunk => {})
-            .on("stderr", chunk => {})
-            .on("close", async code => {
-              process = null;
-              processLogger.info(`Process closing with code ${code}`);
-
-              const stat = await getStat(variable.filename).catch(e => {
-                oakLogger.error(
-                  `Error reading stat for ${
-                    variable.filename
-                  } (2nd attempt): ${e}`
-                );
-                throw e;
-              });
-              rej(stat);
-            })
-            .on("error", () => {
-              process = null;
-              processLogger.error(`Process errored`);
-              rej("error");
-            });
-        });
-      }
-      const updatedDeps = variableDependencies.filter(
-        dep => dep.mtime > stat.mtime
-      );
-      if (updatedDeps.length > 0) {
-        return new Promise(function(res, rej) {
-          oakLogger.debug(
-            `${key} is out of date because ${
-              updatedDeps.length
-            } dependices have updated. Calling recipe ${variable.recipe}`
-          );
-          processLogger.info(`spawning process with "${recipe}"`);
-          process = runRecipe(recipe)
-            .on("stdout", chunk => {})
-            .on("stderr", chunk => {})
-            .on("close", async code => {
-              process = null;
-              //console.log(`child process exited with code ${code}`);
-              processLogger.info(`Process closing with code ${code}`);
-
-              const stat2 = await getStat(variable.filename).catch(e => {
-                oakLogger.error(
-                  `Error reading stat for ${
-                    variable.filename
-                  } (2nd attempt): ${e}`
-                );
-                throw e;
-                return null;
-              });
-              res(stat2);
-            })
-            .on("error", () => {
-              process = null;
-              processLogger.error(`Process errored`);
-              rej("error2");
-            });
-        });
-      }
-      return new Promise(function(res, rej) {
-        res(stat);
-      });
-    });
+    const variableDefinition = createVariableDefinition(variable, key);
+    m.variable(new OakInspector(null, key)).define(
+      key,
+      variable.deps,
+      variableDefinition
+    );
   });
 }
