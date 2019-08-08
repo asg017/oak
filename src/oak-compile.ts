e2eba4b8dc5d6498042b1319416121095bdf36d1
Alex Garcia <alexsebastian.garcia@gmail.com>
2019-08-08 04:41:08 +0000

    more typing

--- src/oak-compile.ts
+++ src/oak-compile.ts
@@ -2,14 +2,63 @@ import { getStat, parseOakfile } from "./utils";
 import FileInfo from "./FileInfo";
 import { dirname, join } from "path";
 import { formatCellName, formatPath } from "./utils";
+import { brotliDecompressSync } from "zlib";
 
 const AsyncFunction = Object.getPrototypeOf(async function() {}).constructor;
 const GeneratorFunction = Object.getPrototypeOf(function*() {}).constructor;
 const AsyncGeneratorFunction = Object.getPrototypeOf(async function*() {})
   .constructor;
 
+type Inspector = {
+  pending: () => void;
+  fulfilled: (value: any) => void;
+  rejected: (error: any) => void;
+};
+type DefineFunctionType = (
+  runtime: any,
+  observer: (name: string) => Inspector | null
+) => any;
+
+type ObservableImportDeclaration = {
+  type: "ImportDeclaration";
+  specifiers: {
+    type: "ImportSpecifier";
+    view: boolean;
+    imported: { type: "Identifier"; name: string };
+    local: { type: "Identifier"; name: string };
+  }[];
+  source: { type: "Literal"; value: string; raw: string };
+  start: number;
+  end: number;
+};
+type ObservableLiteral = {
+  type: "Literal";
+  value: any;
+  raw: string;
+  start: number;
+  end: number;
+};
+type ObservableBlockStatement = {
+  type: "BlockStatement";
+  body: any[];
+  start: number;
+  end: number;
+};
+type ObservableCell = {
+  type: "Cell";
+  id: {
+    type: "Identifier";
+    name: string;
+  } | null;
+  async: boolean;
+  generator: boolean;
+  references: { type: string; name: string }[];
+  body: ObservableLiteral &
+    ObservableImportDeclaration &
+    ObservableBlockStatement;
+};
 export const defineCellImport = async (
-  cell: any,
+  cell: ObservableCell,
   baseModuleDir: string
 ): Promise<{
   names: string[];
@@ -25,7 +74,7 @@ export const defineCellImport = async (
 };
 
 export const defineCell = (
-  cell: any,
+  cell: ObservableCell,
   source: string
 ): { cellFunction: any; cellName: string; cellReferences: string[] } => {
   let name = null;
@@ -104,11 +153,11 @@ export const oakDefine = (
   oakfileModule: any,
   source: string,
   baseModuleDir: string
-): ((runtime: any, observer: any) => any) => {
+): DefineFunctionType => {
   return function define(runtime, observer) {
     const main = runtime.module();
 
-    oakfileModule.cells.map(async cell => {
+    oakfileModule.cells.map(async (cell: ObservableCell) => {
       if (cell.body.type === "ImportDeclaration") {
         const { names, aliases, from } = await defineCellImport(
           cell,
