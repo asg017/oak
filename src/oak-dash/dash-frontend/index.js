f1b4a769ccc41681b6a4233ee90d06e0e6339f95
Alex Garcia <alexsebastian.garcia@gmail.com>
2019-08-13 04:23:32 +0000

    better context, provider handling

--- src/oak-dash/dash-frontend/index.js
+++ src/oak-dash/dash-frontend/index.js
@@ -3,56 +3,110 @@ import { Component } from "preact";
 import { createContext } from "preact-context";
 import io from "socket.io-client";
 
-const OakfileContext = createContext(13);
+export const OakfileContext = createContext();
 
-function X() {
-  return <OakfileContext.Consumer render={um => <div>{um}x</div>} />;
+function OakfileCode() {
+  return (
+    <OakfileContext.Consumer
+      render={oakfile => {
+        console.log(`qaz`, oakfile);
+        if (!oakfile) return <div>No Oakfile...</div>;
+        return (
+          <div className="OakfileCode">
+            <pre>
+              <code>{oakfile.contents}</code>
+            </pre>
+          </div>
+        );
+      }}
+    />
+  );
 }
-export default class App extends Component {
-  render() {
+
+function OakfileGraph() {
+  return (
+    <OakfileContext.Consumer
+      render={oakfile => {
+        if (!oakfile) return <div>No Oakfile...</div>;
+        return (
+          <div className="OakfileGraph">
+            <pre>
+              <code>
+                {oakfile.module.cells.map(cell => (
+                  <span>
+                    {cell.id.name}
+                    {cell.references.map(ref => (
+                      <span>{ref.name}</span>
+                    ))}
+                  </span>
+                ))}
+              </code>
+            </pre>
+          </div>
+        );
+      }}
+    />
+  );
+}
+class SocketProviders extends Component {
+  state = {
+    oakfile: null
+  };
+  socket = null;
+  _onAuth = () => {
+    console.log("auth please");
+    this.socket.on("Oakfile", data => {
+      console.log("new oakfile:", data);
+      this.setState({ oakfile: data });
+    });
+  };
+  _onUnAuth = err => {
+    console.error("unauthorized: ", err.data);
+    throw new Error(err.data.type);
+  };
+  _onConnect = () => {
+    console.log("connected");
     const params = new URL(document.baseURI).searchParams;
     const encodedToken = params.get("token");
     if (!encodedToken) {
       console.error(`need token`);
       throw Error(`needs token`);
     }
-    console.log(params, encodedToken);
     const token = atob(encodedToken);
-    var socket = io.connect("");
-
-    const onConnect = () => {
-      console.log("connected");
-      const onAuth = () => {
-        console.log("auth please");
-        socket.on("Oakfile", data => {
-          console.log("new oakfile:", data);
-        });
-      };
-      const onUnAuth = err => {
-        console.error("unauthorized: ", err.data);
-        throw new Error(err.data.type);
-      };
-      console.log(token);
-      socket.emit("pls");
-      socket
-        .emit("authenticate", { token })
-        .on("authenticated", onAuth)
-        .on("unauthorized", onUnAuth);
-    };
-    socket.on("connect", onConnect);
-    socket.on("disconnect", () => {
-      console.log("socket disconnected");
-    });
-    socket.on("error", () => {
-      console.log("socket error");
-    });
+    this.socket.emit("pls");
+    this.socket
+      .emit("authenticate", { token })
+      .on("authenticated", this._onAuth)
+      .on("unauthorized", this._onUnAuth);
+  };
+  componentDidMount() {
+    this.socket = io.connect("");
+    this.socket.on("connect", this._onConnect);
+    this.socket.on("disconnect", () => console.log("socket disconnected"));
+    this.socket.on("error", () => console.log("socket error"));
+  }
+  render() {
+    console.log(this.state);
+    const { oakfile } = this.state;
     return (
       <div>
-        <h1>Hello, World!</h1>
-        <OakfileContext.Provider value={42}>
-          <X />
-          <div>yeet</div>
+        <OakfileContext.Provider value={oakfile}>
+          {this.props.children}
         </OakfileContext.Provider>
+      </div>
+    );
+  }
+}
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+        <SocketProviders>
+          <OakfileCode />
+          <OakfileGraph />
+        </SocketProviders>
       </div>
     );
   }
