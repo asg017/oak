dfe15264f260f9a1e76dbbf8e5206eb4a57a9f0d
Alex Garcia <alexsebastian.garcia@gmail.com>
2019-08-16 06:42:53 +0000

    checkpoint

--- src/oak-print.ts
+++ src/oak-print.ts
@@ -1,10 +1,18 @@
 import { parseOakfile } from "./utils";
 import { Library } from "./Library";
-import { join } from "path";
+import { join, dirname } from "path";
+import { digraph } from "graphviz";
+import { merge } from "d3-array";
 
 type OakPrintArgumentsType = {
   filename: string;
+  output: string;
 };
+
+type OakPrintOutputType = "stdout" | "png" | "svg";
+
+const outputSet = new Set(["stdout", "png", "svg", "dot"]);
+
 const styles = {
   bgWhite: { open: "\u001b[47m", close: "\u001b[49m" },
   bold: { open: "\u001b[1m", close: "\u001b[22m" },
@@ -16,21 +24,102 @@ const styleVariable = (v: string) =>
     styles.bgWhite.close
   }${styles.black.close}${styles.bold.close}`;
 
-export async function oak_print(args: OakPrintArgumentsType): Promise<void> {
-  const oakfile = await parseOakfile(join(process.cwd(), args.filename));
-  const libSet = new Set(Object.keys(new Library()));
-  console.log(`Oakfile at ${args.filename}:`);
-  oakfile.module.cells.map(cell => {
-    // TODO filename and recipe
-    if (cell.body.type === "ImportDeclaration") {
-      console.warn("WARNING oak print cant print import statements yet");
-    } else {
+const print_dot = (modules, libSet) => {
+  const g = digraph("G");
+  modules.map(module => {
+    module.cells.map((cell, i) => {
+      // TODO filename and recipe
+      if (cell.body.type === "ImportDeclaration") {
+        console.warn("WARNING oak print cant print import statements yet");
+      } else {
+        console.log(
+          `${styleVariable(cell.id.name)} ${cell.references
+            .map(ref => ref.name)
+            .join(",")}`
+        );
+        const cluster = g.addCluster(`cluster_${i}`);
+        const node = cluster.addNode(cell.id.name);
+        cell.references
+          .filter(ref => !libSet.has(ref.name))
+          .map(ref => cluster.addEdge(ref.name, node));
+      }
+    });
+  });
+
+  console.log(g.to_dot());
+  return g;
+};
+
+const print_stdout = (filename, modules: any[], libSet) => {
+  console.log(`Oakfile at ${filename}:`);
+  console.log(modules);
+  modules.map(module => {
+    module.cells.map(cell => {
+      if (cell.body.type === "ImportDeclaration") {
+        console.log(
+          `import ${cell.body.specifiers
+            .map(spec => `${spec.local.name} as ${spec.imported.name}`)
+            .join(",")} from ${cell.body.source.raw}`
+        );
+        return;
+      }
       console.log(
         `${styleVariable(cell.id.name)} ${cell.references
           .map(ref => ref.name)
           .filter((refName: string) => !libSet.has(refName))
           .join(",")}`
       );
-    }
+    });
   });
+};
+
+const parseModules = async (
+  filename: string,
+  parsedOakfileSet?: Set<string>
+): Promise<any[]> => {
+  if (!parsedOakfileSet) {
+    parsedOakfileSet = new Set([]);
+  }
+  if (parsedOakfileSet.has(filename)) {
+    throw Error(
+      `Circular imports. repeated Oakfile: "${filename}". Visited: ${Array.from(
+        parsedOakfileSet
+      )
+        .map(f => `"${f}"`)
+        .join(",")}`
+    );
+  }
+  parsedOakfileSet.add(filename);
+
+  const oakfile = await parseOakfile(filename);
+  const dir = dirname(filename);
+  const importCells = oakfile.module.cells.filter(
+    cell => cell.body.type === "ImportDeclaration"
+  );
+  const importedModules = await Promise.all(
+    importCells.map(importCell => {
+      const path = join(dir, importCell.body.source.value);
+      return parseModules(path, parsedOakfileSet);
+    })
+  );
+  return [oakfile.module, ...merge(importedModules)];
+};
+export async function oak_print(args: OakPrintArgumentsType): Promise<void> {
+  if (!outputSet.has(args.output)) {
+    console.error(
+      `Invalid output parameter: ${args.output} not in ${Array.from(
+        outputSet.values()
+      ).join(",")}`
+    );
+  }
+  const modules = await parseModules(join(process.cwd(), args.filename));
+  const libSet = new Set(Object.keys(new Library()));
+  switch (args.output) {
+    case "stdout":
+      print_stdout(args.filename, modules, libSet);
+      break;
+    case "dot":
+      print_dot(modules, libSet);
+      break;
+  }
 }
