d8060e2491d8feb887d414839d57c332d4d6ce3a
Alex Garcia <alexsebastian.garcia@gmail.com>
2019-09-26 19:38:03 +0000

    updat eca-schools oakfile

--- make_examples/ca-schools/Oakfile.js
+++ make_examples/ca-schools/Oakfile.js
@@ -2,50 +2,50 @@ COUNTY = "Los Angeles";
 
 COUNTY_SNAKE = COUNTY.toLowerCase().replace(" ", "_");
 
-cde_txt = cell({
+recipe cde_txt = task({
   path: "pubschools.txt",
-  recipe: cde_text =>
+  make: cde_text =>
     shell`curl -o ${cde_text} https://raw.githubusercontent.com/datadesk/california-k12-notebooks/master/input/pubschls.txt`
 });
 
-public_schools = cell({
+recipe public_schools = task({
   path: "public_schools.csv",
-  recipe: public_schools =>
+  make: public_schools =>
     shell`pipenv run python clean_cde.py --input=${cde_txt} --output=${public_schools}`
 });
 
-charter_schools = cell({
+recipe charter_schools = task({
   path: "charter_schools.csv",
-  recipe: charter_schools =>
+  make: charter_schools =>
     shell`csvgrep -c is_charter -m Y ${public_schools} > ${charter_schools}`
 });
 
-public_schools_in_county = cell({
+recipe public_schools_in_county = task({
   path: `public_schools_in_${COUNTY_SNAKE}_county.csv`,
-  recipe: public_schools_in_county =>
+  make: public_schools_in_county =>
     shell`csvgrep -c county -m ${COUNTY} ${public_schools} > ${public_schools_in_county}`
 });
 
-charter_schools_in_county = cell({
+recipe charter_schools_in_county = task({
   path: `charter_schools_in_${COUNTY_SNAKE}_county.csv`,
-  recipe: charter_schools_in_county =>
+  make: charter_schools_in_county =>
     shell`csvgrep -c county -m ${COUNTY} ${charter_schools} > ${charter_schools_in_county}`
 });
 
-charter_schools_geo = cell({
+recipe charter_schools_geo = task({
   path: "charter_schools.geojson",
-  recipe: charter_schools_geo =>
+  make: charter_schools_geo =>
     shell`pipenv run python to_geo.py --input=${charter_schools} --output=${charter_schools_geo}`
 });
 
-public_schools_in_county_geo = cell({
+recipe public_schools_in_county_geo = task({
   path: `public_schools_in_${COUNTY_SNAKE}_county.geojson`,
-  recipe: public_schools_in_county_geo =>
+  make: public_schools_in_county_geo =>
     shell`pipenv run python to_geo.py --input=${public_schools_in_county} --output=${public_schools_in_county_geo}`
 });
 
-charter_schools_in_county_geo = cell({
+recipe charter_schools_in_county_geo = task({
   path: `charter_schools_in_${COUNTY_SNAKE}_county.geojson`,
-  recipe: charter_schools_in_county_geo =>
+  make: charter_schools_in_county_geo =>
     shell`pipenv run python to_geo.py --input=${charter_schools_in_county} --output=${charter_schools_in_county_geo}`
 });
