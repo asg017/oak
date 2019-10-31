02e69d871421a6b2637fbbd92fbae19d4a989f3c
Alex Garcia <alexsebastian.garcia@gmail.com>
2019-10-31 23:14:54 +0000

    Examples cleanup

--- make_examples/ca-schools/Oakfile.js
+++ make_examples/ca-schools/Oakfile.js
@@ -2,49 +2,49 @@ COUNTY = "Los Angeles";
 
 COUNTY_SNAKE = COUNTY.toLowerCase().replace(" ", "_");
 
-recipe cde_txt = task({
+cde_txt = task({
   path: "pubschools.txt",
   run: cde_text =>
     shell`curl -o ${cde_text} https://raw.githubusercontent.com/datadesk/california-k12-notebooks/master/input/pubschls.txt`
 });
 
-recipe public_schools = task({
+public_schools = task({
   path: "public_schools.csv",
   run: public_schools =>
     shell`pipenv run python clean_cde.py --input=${cde_txt} --output=${public_schools}`
 });
 
-recipe charter_schools = task({
+charter_schools = task({
   path: "charter_schools.csv",
   run: charter_schools =>
     shell`csvgrep -c is_charter -m Y ${public_schools} > ${charter_schools}`
 });
 
-recipe public_schools_in_county = task({
+public_schools_in_county = task({
   path: `public_schools_in_${COUNTY_SNAKE}_county.csv`,
   run: public_schools_in_county =>
     shell`csvgrep -c county -m ${COUNTY} ${public_schools} > ${public_schools_in_county}`
 });
 
-recipe charter_schools_in_county = task({
+charter_schools_in_county = task({
   path: `charter_schools_in_${COUNTY_SNAKE}_county.csv`,
   run: charter_schools_in_county =>
     shell`csvgrep -c county -m ${COUNTY} ${charter_schools} > ${charter_schools_in_county}`
 });
 
-recipe charter_schools_geo = task({
+charter_schools_geo = task({
   path: "charter_schools.geojson",
   run: charter_schools_geo =>
     shell`pipenv run python to_geo.py --input=${charter_schools} --output=${charter_schools_geo}`
 });
 
-recipe public_schools_in_county_geo = task({
+public_schools_in_county_geo = task({
   path: `public_schools_in_${COUNTY_SNAKE}_county.geojson`,
   run: public_schools_in_county_geo =>
     shell`pipenv run python to_geo.py --input=${public_schools_in_county} --output=${public_schools_in_county_geo}`
 });
 
-recipe charter_schools_in_county_geo = task({
+charter_schools_in_county_geo = task({
   path: `charter_schools_in_${COUNTY_SNAKE}_county.geojson`,
   run: charter_schools_in_county_geo =>
     shell`pipenv run python to_geo.py --input=${charter_schools_in_county} --output=${charter_schools_in_county_geo}`
