88b2de7a795d710a75a41e290c6e4fc068864ef0
Alex Garcia <alexsebastian.garcia@gmail.com>
2019-10-31 23:25:44 +0000

    fix planets oakfile

--- examples/mbostock_planets/Oakfile.js
+++ examples/mbostock_planets/Oakfile.js
@@ -1,21 +1,21 @@
-upr_all = cell({
+upr_all = task({
   path: "phl_hec_all_confirmed.csv",
-  recipe: upr_all => shell`echo "doing upr_all"
+  run: upr_all => shell`echo "doing upr_all"
 curl 'http://www.hpcf.upr.edu/~abel/phl/phl_hec_all_confirmed.csv.zip' -O phl_hec_all_confirmed.csv.zip 
 unzip -u phl_hec_all_confirmed.csv.zip
 mv phl_hec_all_confirmed.csv ${upr_all}`
 });
 
-planets = cell({
+planets = task({
   path: "planets.csv",
-  recipe: planets =>
+  run: planets =>
     shell`echo "doing planets"
 wget -O ${planets} https://gist.github.com/mbostock/3007180/raw/79339d19b6c9fea256ab9e99f7f0be18372904bf/planets.csv`
 });
 
-exoplanets = cell({
+exoplanets = task({
   path: "exoplanets.csv",
-  recipe: exoplanets => shell`# echo "doing exoplanets"
+  run: exoplanets => shell`# echo "doing exoplanets"
 # cp -f ${planets} ${exoplanets}
 cut -d, -f1,12,46 ${upr_all} | tail -n+2 >> ${exoplanets}`
 });
