ae614b8b4063b7894eb93e865e9d24965aa2481d
Alex Garcia <alexsebastian.garcia@gmail.com>
2020-03-19 03:56:03 +0000

    updates

--- src/commands/dash/dash-frontend/components/TaskGraphControls.js
+++ src/commands/dash/dash-frontend/components/TaskGraphControls.js
@@ -4,15 +4,35 @@ export default class TaskGraphControls extends Component {
     const { controls, onUpdate } = this.props;
     return (
       <div>
-        <div>rankdir</div>
-        <select
-          onChange={e =>
-            onUpdate(Object.assign(controls, { rankdir: e.target.value }))
-          }
-        >
-          <option>TB</option>
-          <option>LR</option>
-        </select>
+        <div>
+          <div>rankdir</div>
+          <select
+            onChange={e =>
+              onUpdate(Object.assign(controls, { rankdir: e.target.value }))
+            }
+          >
+            <option>TB</option>
+            <option>LR</option>
+          </select>
+        </div>
+        <div>
+          <div>align</div>
+          <select
+            onChange={e =>
+              onUpdate(
+                Object.assign(controls, {
+                  align: e.target.value === "none" ? null : e.target.value,
+                })
+              )
+            }
+          >
+            <option>none</option>
+            <option>UL</option>
+            <option>UR</option>
+            <option>DL</option>
+            <option>DR</option>
+          </select>
+        </div>
       </div>
     );
   }
