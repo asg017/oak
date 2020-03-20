1656eebf16638b912f50edbe86e98abb9304f943
Alex Garcia <alexsebastian.garcia@gmail.com>
2020-03-20 02:35:52 +0000

    automatically updating dashboard

--- src/commands/dash/dash-frontend/components/App.js
+++ src/commands/dash/dash-frontend/components/App.js
@@ -1,15 +1,9 @@
 import { h, Component } from "preact";
 import Header from "./Header";
 import AppSection from "./AppSection";
-import io from "socket.io-client";
 
 export default class App extends Component {
-  componentDidMount() {
-    const socket = io.connect("/");
-    socket.on("oakfile", data => {
-      console.log("socket.on oakfile", data);
-    });
-  }
+  componentDidMount() {}
   render() {
     return (
       <div>
