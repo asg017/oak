37297f83ad8641dfabeb3ab2797780b8d7d3e97d
Alex Garcia <alexsebastian.garcia@gmail.com>
2020-03-22 04:55:24 +0000

    more to new sqlite lib

--- src/db.ts
+++ src/db.ts
@@ -1,7 +1,8 @@
-import sqlite3, { Database } from "sqlite3";
+import sqlite, { Database } from "sqlite";
 import { dirname, join } from "path";
 import { mkdirsSync, existsSync } from "fs-extra";
 import { ParseOakfileResults, hashString } from "./utils";
+import SQL from "sql-template-strings";
 
 type DBOakfile = {
   hash: string;
@@ -20,36 +21,29 @@ export class OakDB {
   constructor(oakfilePath: string) {
     const oakMetedataDir = join(dirname(oakfilePath), ".oak");
     mkdirsSync(oakMetedataDir);
-
     this.dbPath = join(oakMetedataDir, "oak.db");
+  }
 
+  async getDb(): Promise<Database> {
     const dbExists = existsSync(this.dbPath);
+    const db = await sqlite.open(this.dbPath, { promise: Promise });
     if (!dbExists) {
-      const db = this.getDb();
-      initDb(db);
-      db.close();
+      await initDb(db);
     }
+    return db;
   }
 
-  getDb(): sqlite3.Database {
-    return new (sqlite3.verbose().Database)(this.dbPath);
-  }
   async addLog(
     runHash: string,
     cellName: string,
     logPath: string,
     time: number
   ) {
-    const db = this.getDb();
-    db.serialize(() => {
-      db.run(
-        `INSERT INTO Logs VALUES (?, ?, ?, ?)`,
-        [runHash, cellName, logPath, time],
-        (err, data) => {
-          db.close();
-        }
-      );
-    });
+    const db = await this.getDb();
+    await db.run(
+      SQL`INSERT INTO Logs VALUES (${runHash}, ${cellName}, ${logPath}, ${time})`
+    );
+    await db.close();
   }
   async registerOakfile(
     oakfileHash: string,
@@ -65,31 +59,17 @@ export class OakDB {
     }
   }
   async getOakfile(oakfileHash: string): Promise<DBOakfile> {
-    const db = this.getDb();
-    return new Promise((resolve, reject) => {
-      db.get(
-        `SELECT hash, mtime FROM Oakfiles WHERE hash=?`,
-        [oakfileHash],
-        (err, row) => {
-          db.close();
-          if (err) reject(err);
-          else resolve(row);
-        }
-      );
-    });
+    const db = await this.getDb();
+    const row = await db.get(
+      SQL`SELECT hash, mtime FROM Oakfiles WHERE hash = ${oakfileHash}`
+    );
+    await db.close();
+    return row;
   }
   async addOakfile(oakfileHash: string, mtime: number) {
-    const db = this.getDb();
-    db.serialize(() => {
-      db.run(
-        `INSERT INTO Oakfiles VALUES (?, ?)`,
-        [oakfileHash, mtime],
-        (err, data) => {
-          console.log("xxx", err, data);
-          db.close();
-        }
-      );
-    });
+    const db = await this.getDb();
+    await db.run(SQL`INSERT INTO Oakfiles VALUES (${oakfileHash}, ${mtime})`);
+    await db.close();
   }
   async addRun(
     oakfileHash: string,
@@ -97,56 +77,45 @@ export class OakDB {
     mtime: number,
     args: string
   ) {
-    const db = this.getDb();
-    return new Promise((resolve, reject) => {
-      db.serialize(() => {
-        db.run(
-          `INSERT INTO Runs VALUES (?, ?, ?, ?)`,
-          [runHash, oakfileHash, mtime, args],
-          (err, data) => {
-            db.close();
-            if (err) reject(err);
-            resolve(data);
-          }
-        );
-      });
-    });
+    const db = await this.getDb();
+    await db.run(
+      SQL`INSERT INTO Runs VALUES (${runHash}, ${oakfileHash}, ${mtime}, ${args})`
+    );
+    await db.close();
   }
   async addCells(oakfileHash: string, parseResults: ParseOakfileResults) {
-    const db = this.getDb();
-    return new Promise((resolve, reject) => {
-      db.serialize(() => {
-        for (let cell of parseResults.module.cells) {
-          if (cell?.id?.name) {
-            const cellName = cell.id.name;
-            const cellRefs = cell.references.map(ref => ref.name);
-            const cellContents = cell.input.substring(cell.start, cell.end);
-            const hashContents = `${cellName}${cellContents}`;
-            const hash = hashString(hashContents);
-            db.run(
-              `INSERT INTO Cells VALUES (?, ?, ?, ?)`,
-              [oakfileHash, hash, cellName, JSON.stringify(cellRefs)],
-              (err, data) => {
-                if (err) reject(err);
-                else resolve(data);
-              }
-            );
-          }
-        }
-        db.close();
-      });
-    });
+    const db = await this.getDb();
+    const cellsWithNames = parseResults.module.cells.filter(
+      cell => cell?.id?.name
+    );
+    await Promise.all(
+      cellsWithNames.map(cell => {
+        const cellName = cell.id.name;
+        const cellRefs = cell.references.map(ref => ref.name);
+        const cellContents = cell.input.substring(cell.start, cell.end);
+        const hashContents = `${cellName}${cellContents}`;
+        const hash = hashString(hashContents);
+        return db.run(
+          SQL`INSERT INTO Cells VALUES (${oakfileHash}, ${hash}, ${cellName}, ${JSON.stringify(
+            cellRefs
+          )})`
+        );
+      })
+    );
+    db.close();
   }
 }
-function initDb(db: sqlite3.Database) {
-  db.serialize(() => {
-    db.run(
-      `CREATE TABLE Oakfiles(
+async function initDb(db: Database) {
+  console.log("initting");
+  await db.run(
+    `CREATE TABLE Oakfiles(
           hash TEXT PRIMARY KEY, 
           mtime INTEGER,
           UNIQUE(hash)
       ); `
-    );
+  );
+  console.log("initting 2");
+  await Promise.all([
     db.run(
       `CREATE TABLE Cells(
             oakfile TEXT,
@@ -155,7 +124,7 @@ function initDb(db: sqlite3.Database) {
             refs TEXT,
             FOREIGN KEY (oakfile) REFERENCES Oakfiles(hash)
         ); `
-    );
+    ),
     db.run(
       `CREATE TABLE Runs(
             hash TEXT PRIMARY KEY,
@@ -163,23 +132,27 @@ function initDb(db: sqlite3.Database) {
             time INTEGER,
             arguments TEXT,
             FOREIGN KEY (oakfile) REFERENCES Oakfiles(hash)        ); `
-    );
+    ),
+  ]);
+  console.log("initting 3");
+  await Promise.all([
     db.run(
       `CREATE TABLE Logs(
-          run TEXT,
-          cellName TEXT,
-          path TEXT,
-          time INTEGER,
-          FOREIGN KEY (run) REFERENCES Runs(hash)
-      ); `
-    );
+            run TEXT,
+            cellName TEXT,
+            path TEXT,
+            time INTEGER,
+            FOREIGN KEY (run) REFERENCES Runs(hash)
+        ); `
+    ),
     db.run(
       `CREATE TABLE Events(
-          run INTEGER,
-          type TEXT,
-          time INTEGER,
-          FOREIGN KEY (run) REFERENCES Runs(hash)
-      ); `
-    );
-  });
+            run INTEGER,
+            type TEXT,
+            time INTEGER,
+            FOREIGN KEY (run) REFERENCES Runs(hash)
+        ); `
+    ),
+  ]);
+  return db;
 }
