4498920b2c444af88af289d79f54bccbf9d5a0a1
Alex Garcia <alexsebastian.garcia@gmail.com>
2020-03-28 20:27:28 +0000

    remove decorators directory.

--- src/commands/pulse.ts
+++ src/commands/pulse.ts
@@ -1,10 +1,16 @@
 import { Runtime } from "@observablehq/runtime";
 import { OakCompiler } from "../oak-compile";
 import { Library } from "../Library";
-import { default as pulseCellDecorator } from "../decorators/pulse";
 import pino from "pino";
 import { fileArgument } from "../cli-utils";
-import { bytesToSize, duration, OakCell } from "../utils";
+import {
+  bytesToSize,
+  duration,
+  OakCell,
+  getStat,
+  CellSignature,
+} from "../utils";
+import Task from "../Task";
 
 const logger = pino();
 
@@ -22,6 +28,87 @@ type PulseTaskResult = {
 type PulseResults = {
   tasks: PulseTaskResult[];
 };
+
+function pulseCellDecorator(
+  cellFunction: (...args: any) => any,
+  cellName: string,
+  cellReferences: string[],
+  cellHashMap: Map<string, CellSignature>,
+  baseModuleDir: string
+): (...any) => any {
+  return async function(...dependencies) {
+    let taskDependents: number[] = [];
+    dependencies.map((dependency, i) => {
+      if (dependency.__task) {
+        taskDependents.push(i);
+      }
+    });
+    let currCell = await cellFunction(...dependencies);
+
+    if (currCell instanceof Task) {
+      await currCell.updateBasePath(baseModuleDir);
+
+      const watchFiles = currCell.watch;
+      const watchStats = await Promise.all(
+        watchFiles.map(async watchFile => {
+          const stat = await getStat(watchFile);
+          return { path: watchFile, stat };
+        })
+      );
+
+      // assuming that the library passed into this runtime
+      // isnt running the tasks, but actually returning the "type" of tasks
+      const taskType = await currCell.runTask();
+      if (currCell.stat === null) {
+        return {
+          __task: true,
+          name: cellName,
+          taskDeps: taskDependents.map(i => cellReferences[i]),
+          target: currCell.target,
+          mtime: 0,
+          type: taskType,
+          bytes: 0,
+          status: "dne",
+        };
+      }
+
+      const outOfDateTaskDependencies = taskDependents.filter(
+        i =>
+          currCell.stat.mtime.getTime() <= dependencies[i].mtime ||
+          dependencies[i].status !== "up"
+      );
+      const outOfDateWatchFiles = watchStats.filter(
+        ({ stat }) => currCell.stat.mtime.getTime() <= stat.mtime
+      );
+      if (
+        outOfDateTaskDependencies.length > 0 ||
+        outOfDateWatchFiles.length > 0
+      ) {
+        return {
+          __task: true,
+          name: cellName,
+          taskDeps: taskDependents.map(i => cellReferences[i]),
+          target: currCell.target,
+          mtime: currCell.stat.mtime.getTime(),
+          type: taskType,
+          bytes: currCell.stat.size,
+          status: "out",
+        };
+      }
+      return {
+        __task: true,
+        name: cellName,
+        taskDeps: taskDependents.map(i => cellReferences[i]),
+        target: currCell.target,
+        mtime: currCell.stat.mtime.getTime(),
+        type: taskType,
+        bytes: currCell.stat.size,
+        status: "up",
+      };
+    }
+    return currCell;
+  };
+}
 
 export async function getPulse(oakfilePath: string): Promise<PulseResults> {
   const runtime = new Runtime(
--- tests/print/test.ts
+++
@@ -1,8 +0,0 @@
-import test from "tape";
-import { oak_print } from "../../src/commands/print";
-import { envFile } from "../utils";
-
-test("oak-print dot", async t => {
-  //const result = await oak_print({ filename: "", output: "dot" });
-  t.end();
-});
