36612e50bbb66f1648ad1498c1ab366eeaeb31e9
Alex Garcia <alexsebastian.garcia@gmail.com>
2020-03-29 05:25:33 +0000

    expose getPulse in oak_pulse.

--- oak-core/src/index.js
+++ oak-core/src/index.js
@@ -2,4 +2,4 @@ export { oak_run } from "./commands/run";
 export { oak_clean } from "./commands/clean";
 export { oak_init } from "./commands/init";
 export { oak_logs } from "./commands/logs";
-export { oak_pulse } from "./commands/pulse";
+export { getPulse, oak_pulse } from "./commands/pulse";
