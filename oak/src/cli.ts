123dcb2c3489b713596b5521771acb7c8cc0a491
Alex Garcia <alexsebastian.garcia@gmail.com>
2020-03-29 07:18:32 +0000

    move top level oak to oak/.

---
+++ oak/src/cli.ts
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+
+import { main } from "@alex.garcia/oak-cli";
+main();
--- src/commands/dash/dash-frontend/utils/api.js
+++
@@ -1,8 +0,0 @@
-const BASE = "";
-export function getMeta() {
-  return fetch(`${BASE}/api/meta`).then(r => r.json());
-}
-
-export function getPulse() {
-  return fetch(`${BASE}/api/pulse`).then(r => r.json());
-}
